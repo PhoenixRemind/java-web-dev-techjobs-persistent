bee55b47db7e57f81561fb824582baf6a3e2281e
Paul <pdeyarmin@gmail.com>
2020-05-26 16:01:10 -0400

    Finished part 2 completely, program accepts inputs and added SQL test 2

--- queries.sql
+++ queries.sql
@@ -3,19 +3,22 @@
 SELECT * FROM techjobs.job;
 
 SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE table_name = 'job' AND COLUMN_NAME = 'id';
+WHERE table_name = 'job' AND COLUMN_NAME = 'id';
 
-  SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE table_name = 'job' AND COLUMN_NAME = 'employer';
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'job' AND COLUMN_NAME = 'employer';
 
-  SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE table_name = 'job' AND COLUMN_NAME = 'name';
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'job' AND COLUMN_NAME = 'name';
 
-  SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE table_name = 'job' AND COLUMN_NAME = 'skills';
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'job' AND COLUMN_NAME = 'skills';
 
 --  id = int / employer, name, skills = varchar
+
 ## Part 2: Test it with SQL
+
+SELECT name FROM employer WHERE location='St. Louis City';
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,12 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Skill extends AbstractEntity {
 
-    @Max(500)
+    @Size(min = 2, max = 500, message = "Please enter a description without exceeding the 500 character limit.")
     private String description;
 
     public Skill() {}
