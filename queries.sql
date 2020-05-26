dd6dff58f8fb228b4a1f06935af5c2c103cea87f
Paul <pdeyarmin@gmail.com>
2020-05-26 10:34:54 -0400

    Completed Part 1 with SQL queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,20 @@
 ## Part 1: Test it with SQL
 
+SELECT * FROM techjobs.job;
+
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE table_name = 'job' AND COLUMN_NAME = 'id';
+
+  SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE table_name = 'job' AND COLUMN_NAME = 'employer';
+
+  SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE table_name = 'job' AND COLUMN_NAME = 'name';
+
+  SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE table_name = 'job' AND COLUMN_NAME = 'skills';
+
+--  id = int / employer, name, skills = varchar
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
